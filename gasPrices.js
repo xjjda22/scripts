7c540bce1d75ef4df07a7633e8794efbd1390a9f
harryranakl <ranahari@gmail.com>
2021-10-01 17:56:58 +0800

    updated:gas prices

---
+++ gasPrices.js
@@ -0,0 +1,40 @@
+// gasPrices
+const axios = require('axios');
+const { inspect }  = require('util');
+
+const deepLogs = (obj) => {
+  return inspect(obj, {depth: 5});
+}
+
+const gasPrices = async (api, debug) => {
+  
+  const api_url = `${api}`;
+
+  const config = {
+    timeout: 30000,
+    url: api_url,
+    method: 'get',
+    responseType: 'json'
+  };
+  const res = await axios(config);
+  const data = res.data;
+  if(debug) console.log('data -- ',api, data);
+  return data;
+}
+
+// console.log('start --');
+gasPrices("https://ethgasstation.info/json/ethgasAPI.json", true);
+gasPrices("https://gasprice.poa.network/", true);
+gasPrices("https://www.gasnow.org/api/v3/gas/price", true);
+gasPrices("https://safe-relay.gnosis.io/api/v1/gas-station/", true);
+gasPrices("https://api.txprice.com/", true);
+gasPrices("https://api.metaswap.codefi.network/gasPrices", true);
+gasPrices("https://www.etherchain.org/api/gasPriceOracle", true);
+//GasNow
+//MyCrypto
+//etherscan
+
+module.exports = {
+  gasPrices
+}
+return;
